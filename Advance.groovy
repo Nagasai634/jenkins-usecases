8cc11fe26c1bdb72235a4543afb2e49e03022867
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-10-31 18:48:37 +0530

    Add Jenkins pipeline for Terraform actions

---
+++ Advance.groovy
@@ -0,0 +1,40 @@
+pipeline {
+    agent any
+    parameters {
+        choice(
+            name: 'TERRAFORM_ACTION',
+            choices: ['apply', 'destroy'],
+            description: 'Choose Terraform action: apply to create, destroy to remove infrastructure'
+        )
+    }
+    stages {
+        stage("Terraform Init") {
+            steps {
+                dir("/home/ansible/jenkins-usecases") {
+                    sh "terraform init"
+                }
+            }
+        }
+        stage("Terraform Plan/Apply") {
+            when {
+                expression { params.TERRAFORM_ACTION == 'apply' }
+            }
+            steps {
+                dir("/home/ansible/jenkins-usecases") {
+                    sh "terraform plan"
+                    sh "terraform apply --auto-approve"
+                }
+            }
+        }
+        stage("Terraform Destroy") {
+            when {
+                expression { params.TERRAFORM_ACTION == 'destroy' }
+            }
+            steps {
+                dir("/home/ansible/jenkins-usecases") {
+                    sh "terraform destroy --auto-approve"
+                }
+            }
+        }
+    }
+}
